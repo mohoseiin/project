f9e41d8c5cf7061df8f155043818a3a29565f3a1
mohammad <mo.hoseiin@gmail.com>
2024-06-04 20:09:59 +0330

    update project sqlite in python

--- Mycv/cv/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Cv
-
-# Register your models here.
-class CvAdmin(admin.ModelAdmin):
-  list_display = ("name" , "email" , "phone", "joined_date",)
-
-admin.site.register(Cv,CvAdmin)
